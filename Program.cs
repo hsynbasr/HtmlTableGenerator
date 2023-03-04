a68993bfb7035bae0ac9808baa5a31d3956eadb2
Hüseyin Başer <hbaser@wissen.com>
2023-03-04 14:55:19 +0300

    JsonReader

--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using System.Text.Json;
+
+Setup();
+
+
+
+
+
+
+
+
+
+void Setup()
+{
+    using var JsonFileReader = File.OpenText("movies.json");
+    var result = JsonSerializer.Deserialize<Movie[]>(JsonFileReader.ReadToEnd(), new JsonSerializerOptions
+    {
+        PropertyNameCaseInsensitive = true
+    });
+
+
+    foreach (var item in result)
+    {
+        System.Console.WriteLine(item.Title);
+    }
+}
