30e5879eaf00173b8c874a57342b97c61df9a8d6
Hüseyin Başer <hbaser@wissen.com>
2023-03-04 14:53:17 +0300

    Movie Class Eklendi

---
+++ Movie.cs
@@ -0,0 +1,8 @@
+class Movie
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Rating { get; set; }
+    public string Genre { get; set; }
+    public int Duration { get; set; }
+}
